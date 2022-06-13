bbd88fa3f051ca42bc35c779774c1f8af03e867a
Lin Ben Moyal <linbm@boaelite.local>
2022-06-13 18:54:04 +0300

    add json

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("hello");
+const express = require("express");
+const app = express();
+
+app.use(express.json())
+
+app.get("/", function (req, res) {
+  console.log("hey");
+  res.send("Hello World");
+});
+
+app.get("/users", function (req, res) {
+  //   res.send('Hello World')
+  console.log("users");
+  res.send("Hello World users");
+});
+
+
+app.post("/users", function (req, res) {
+   
+    // console.log("post users", req);
+    console.log("body:",req.body)
+    res.send("Hello World post users");
+  });
+
+console.log("my app is listinning")
+ app.listen(3000);
+  
